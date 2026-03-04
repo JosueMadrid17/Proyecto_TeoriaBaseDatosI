af15488b85553dc46d2ecb6a543610d458785596
rigobertomadrid <josue_madrid@unitec.edu>
2026-03-03 22:50:48 -0600

    Actualización del SQL
    Se actualizan algunos tipos de datos y en algunos campos la cantidad.

--- docs/Proyecto_PresupuestoPersonal.sql
+++ docs/Proyecto_PresupuestoPersonal.sql
@@ -1,64 +1,88 @@
 CREATE TABLE `usuario` (
   `id_usuario` int PRIMARY KEY,
-  `primer_nombre` varchar(300),
-  `segundo_nombre` varchar(300),
-  `primer_apellido` varchar(300),
-  `segundo_apellido` varchar(300),
-  `correo_electronico` varchar(300),
-  `salario_mensual` numeric(30,5),
-  `estado_usuario` bool
+  `primer_nombre` varchar(100),
+  `segundo_nombre` varchar(100),
+  `primer_apellido` varchar(100),
+  `segundo_apellido` varchar(100),
+  `correo_electronico` varchar(100),
+  `clave` varchar(100),
+  `salario_mensual` decimal(20,5),
+  `estado_usuario` bool,
+  `creado_en` timestamp,
+  `modificado_en` timestamp,
+  `creado_por` varchar(100),
+  `modificado_por` varchar(100)
 );
 
 CREATE TABLE `presupuesto` (
   `id_presupuesto` int PRIMARY KEY,
   `id_usuario` int,
-  `nombre` varchar(300),
+  `nombre` varchar(100),
   `anio_inicio` int,
   `mes_inicio` int,
   `anio_fin` int,
   `mes_fin` int,
-  `total_ingresos` numeric(30,5),
-  `total_gastos` numeric(30,5),
-  `total_ahorros` numeric(30,5),
-  `fecha_creacion` datetime,
-  `hora_creacion` time,
-  `estado_presupuesto` bool
+  `total_ingresos` decimal(20,5),
+  `total_gastos` decimal(20,5),
+  `total_ahorros` decimal(20,5),
+  `fecha_hora_creacion` timestamp,
+  `estado_presupuesto` bool,
+  `creado_en` timestamp,
+  `modificado_en` timestamp,
+  `creado_por` varchar(100),
+  `modificado_por` varchar(100)
 );
 
 CREATE TABLE `categoria` (
   `id_categoria` int PRIMARY KEY,
-  `nombre` varchar(300),
-  `descripcion_detallada` varchar(300),
-  `tipo_categoria` varchar(30)
+  `nombre` varchar(100),
+  `descripcion_detallada` varchar(100),
+  `tipo_categoria` varchar(40),
+  `creado_en` timestamp,
+  `modificado_en` timestamp,
+  `creado_por` varchar(100),
+  `modificado_por` varchar(100)
 );
 
 CREATE TABLE `subcategoria` (
   `id_subcategoria` int PRIMARY KEY,
   `id_categoria` int,
-  `nombre` varchar(300),
+  `nombre` varchar(100),
   `descripcion_detallada` varchar(300),
   `activo` bool,
-  `es_por_defecto` int
+  `es_por_defecto` int,
+  `creado_en` timestamp,
+  `modificado_en` timestamp,
+  `creado_por` varchar(100),
+  `modificado_por` varchar(100)
 );
 
 CREATE TABLE `presupuesto_detalle` (
   `id_detalle` int PRIMARY KEY,
   `id_presupuesto` int,
   `id_subcategoria` int,
-  `monto_mensual_asignado` numeric(30,5),
-  `observaciones` varchar(300)
+  `monto_mensual_asignado` decimal(20,5),
+  `observaciones` varchar(300),
+  `creado_en` timestamp,
+  `modificado_en` timestamp,
+  `creado_por` varchar(100),
+  `modificado_por` varchar(100)
 );
 
 CREATE TABLE `obligacion_fija` (
   `id_obligacion` int PRIMARY KEY,
   `id_subcategoria` int,
-  `nombre` varchar(300),
+  `nombre` varchar(100),
   `descripcion_detallada` varchar(300),
-  `monto_fijo_mensual` numeric(30,5),
+  `monto_fijo_mensual` decimal(20,5),
   `dia_vencimiento` int,
   `vigente` bool,
-  `fecha_inicio` datetime,
-  `fecha_fin` datetime
+  `fecha_inicio` timestamp,
+  `fecha_fin` timestamp,
+  `creado_en` timestamp,
+  `modificado_en` timestamp,
+  `creado_por` varchar(100),
+  `modificado_por` varchar(100)
 );
 
 CREATE TABLE `transaccion` (
@@ -66,16 +90,18 @@ CREATE TABLE `transaccion` (
   `id_detalle` int,
   `anio_transaccion` int,
   `mes_transaccion` int,
-  `id_obligacion` int,
-  `tipo_transaccion` varchar(30),
+  `tipo_transaccion` varchar(40),
   `descripcion` varchar(300),
-  `monto_transaccion` numeric(30,5),
+  `monto_transaccion` decimal(20,5),
   `fecha_transaccion` datetime,
-  `metodo_pago` varchar(50),
+  `metodo_pago` varchar(40),
   `num_factura` int,
   `observaciones` varchar(300),
-  `fecha_registro` datetime,
-  `hora_registro` time
+  `fecha_hora_registro` timestamp,
+  `creado_en` timestamp,
+  `modificado_en` timestamp,
+  `creado_por` varchar(100),
+  `modificado_por` varchar(100)
 );
 
 CREATE TABLE `obligacionfija_transaccion` (
@@ -95,6 +121,6 @@ ALTER TABLE `obligacion_fija` ADD FOREIGN KEY (`id_subcategoria`) REFERENCES `su
 
 ALTER TABLE `transaccion` ADD FOREIGN KEY (`id_detalle`) REFERENCES `presupuesto_detalle` (`id_detalle`);
 
-ALTER TABLE `obligacionfija_transaccion` ADD FOREIGN KEY (`id_transaccion`) REFERENCES `obligacion_fija` (`id_obligacion`);
+ALTER TABLE `obligacionfija_transaccion` ADD FOREIGN KEY (`id_transaccion`) REFERENCES `transaccion` (`id_transaccion`);
 
-ALTER TABLE `obligacionfija_transaccion` ADD FOREIGN KEY (`id_obligacion`) REFERENCES `transaccion` (`id_transaccion`);
+ALTER TABLE `obligacionfija_transaccion` ADD FOREIGN KEY (`id_obligacion`) REFERENCES `obligacion_fija` (`id_obligacion`);
